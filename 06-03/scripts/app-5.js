6a224244bafeb412dfbb20761b402a2f9e4f5b58
JeanLLopes <jean.llopes@outlook.com>
2017-02-03 21:49:12 -0200

    Update class 06 Add Other way to class 06

---
+++ 06-03/scripts/app-5.js
@@ -0,0 +1,50 @@
+(function() {
+    angular
+        .module('AngularAppModule',[])
+        .controller('ClienteController', ClienteController)
+
+        function ClienteController(){
+            //SETAMOS AQUI NOSSA VARIAVEL QU VAI PEGAR A VIEWMODEL
+            //E PARA NÃO FICAR TRABALHANDO COM this A TODO MOMENTO
+            //POIS SEUS VALORES SÃ MUTAVEIS
+            //VAMOS DEFINIR UMA VARIAVEL PARA ISSO
+            var vmcliente = this
+
+            //AQUI AO INVES DE USAR $SCOPE USAMOS A NOSSA VIEWMODEL
+            vmcliente.titulo = "Sistema de Cadastro Em Angular"
+            vmcliente.listaClientes = [
+                {Id: 2, Nome: "Jose 1", Email: "jose@gmail.com"},
+                {Id: 1, Nome: "Maria 2", Email: "Maria@gmail.com"},
+                {Id: 3, Nome: "Vitor 3", Email: "vitor@gmail.com"},
+                {Id: 4, Nome: "Paulo 4", Email: "paulo@gmail.com"},
+            ]
+
+            vmcliente.AdicionarCliente = function(dadosCliente){
+                debugger;
+                dadosCliente.Id = vmcliente.listaClientes.length + 1; 
+                vmcliente.listaClientes.push(angular.copy(dadosCliente));
+
+                //LIMPA OS CAMPOS DE BUSCA DO NAVEGADOR
+                ClearInputsFields();
+            }
+
+            vmcliente.Remover = function(cliente){
+                vmcliente.listaClientes = cliente.filter(function(item){
+                    debugger;
+                    return !item.selecionado
+                })
+            }
+        }
+
+
+        function ClearInputsFields(){
+            var inputsFields = document.querySelectorAll("input[type='text'], input[type='email']");
+            inputsFields.forEach(function(inputsField) {
+                 inputsField.value = '';
+            }, this);
+        }
+
+        //AQUI NOS INJETAMOS O SCOPE
+        //QUANDO USAMOS CONTROLLER AS "" NÃO PRECISAMO MAIS USAR O $SCOPE
+        //AngularAppController["$inject"] = ["$scope"];
+})();
