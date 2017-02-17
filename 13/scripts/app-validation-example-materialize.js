3be7b2155893e1dff963b93625e0cfd683de9ea8
JeanLLopes <jean.llopes@outlook.com>
2017-02-17 11:02:03 -0200

    Add new example with NGMessage

---
+++ 13/scripts/app-validation-example-materialize.js
@@ -0,0 +1,11 @@
+(function(){
+    angular
+        .module('AngularModule',['ngMessages'])
+        .controller('AngularNgMessageController', AngularNgMessageController)
+
+    function AngularNgMessageController(){
+        var vmAngularNgMessage = this;
+
+
+    }
+})()
